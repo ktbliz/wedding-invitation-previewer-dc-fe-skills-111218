fd9dc2d33cdc4e52a0c544aacb3c73899e8c39f3
Katie Blizzard <kblizzard11@gmail.com>
2019-01-29 03:49:33 +0000

    Automatically backed up by Learn

--- invite-design.js
+++ invite-design.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const invite = document.getElementById("invite")
     invite.style.backgroundColor = "#FFEAAA";
     invite.style.border = "thick solid #2A92CE";
-    invite.style.fontFamily = "Parisienne, cursive";
+    invite.style.fontFamily = "Damion, cursive";
     document.getElementById("invite-text").style.color = "#2A92CE";
   })
   
